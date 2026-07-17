eff5807505354d5f33dd8a6a113dedd60e3e3aef
kiranjajal136 <167615806+kiranjajal136@users.noreply.github.com>
2026-07-17 18:33:00 +0530

    Add: meeting-schedule - submission-2

---
+++ Data Structures & Algorithms/meeting-schedule/submission-2.js
@@ -0,0 +1,24 @@
+/**
+ * Definition of Interval:
+ * class Interval {
+ *   constructor(start, end) {
+ *     this.start = start;
+ *     this.end = end;
+ *   }
+ * }
+ */
+
+class Solution {
+    /**
+     * @param {Interval[]} intervals
+     * @returns {boolean}
+     */
+    canAttendMeetings(intervals) {
+        let sorted = intervals.sort((a,b) => a.start-b.start);
+
+        for(let i=1;i<sorted.length;i++) {
+            if(sorted[i].start < sorted[i-1].end) return false;
+        }
+        return true;
+    }
+}
